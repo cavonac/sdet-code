eeb27dae4e4e3bedc7ff0603eb0b1988b0988369
anonymous user <anonymous@localhost.localdomain>
2018-10-10 15:02:08 -0700

    more tests

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # sdet-code
 Demonstration of code that would be useful for SDETs to learn
+
+## Python
+Learning Python from https://www.python.org/
+
+### PyTest
+See https://www.pytest.org/
--- test_pytest.py
+++ test_pytest.py
@@ -1,13 +1,21 @@
 def fibo(n):
+    # Outputs a list of numbers up to and including the given number: n
     a, b = 0, 1
     list = []
-    while a < n:
+    while a <= n:
         list.append(a)
         a, b = b, a+b
     return list
 
 def test_fibo_1():
-    assert fibo(1) == [0]
+    assert fibo(1) == [0, 1, 1]
 
 def test_fibo_10():
     assert fibo(10) == [0, 1, 1, 2, 3, 5, 8]
+
+def test_fibo_9():
+    assert fibo(9) == [0, 1, 1, 2, 3, 5, 8]
+
+def test_fibo_13():
+    assert fibo(13) == [0, 1, 1, 2, 3, 5, 8, 13]
+
