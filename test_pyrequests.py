aa952e9c7030d992a952c5f6598dd541e1b67b50
anonymous user <anonymous@localhost.localdomain>
2018-10-10 15:54:11 -0700

    .

--- test_pyrequests.py
+++ test_pyrequests.py
@@ -6,5 +6,5 @@ def test_gitevents():
 
 def test_XFrameOptions():
     # Checking that OWASP.org sets the X-Frame-Options header correctly to deny clickjacking
-    r = requests.get('http://www.owasp.org/')
+    r = requests.get('https://www.owasp.org/')
     assert dict(r.headers).get("X-Frame-Options") == 'DENY'
