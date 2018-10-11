aa253514b6c9bd5825f40245becfc7b4fccfe54a
anonymous user <anonymous@localhost.localdomain>
2018-10-10 22:29:26 -0700

    added xcontenttypeoptions test

--- test_pyrequests.py
+++ test_pyrequests.py
@@ -1,8 +1,26 @@
 import requests
+
 def getXFrameOptions(url):
+    """Retrieves the X-Frame-Options header from an URL string"""
     r = requests.get(url)
     return dict(r.headers).get("X-Frame-Options")
 
+def getXXssProtection(url):
+    """Retrieves the X-XSS-Protection header from an URL string"""
+    r = requests.get(url)
+    return dict(r.headers).get("X-XSS-Protection")
+
+def getXContentTypeOptions(url):
+    """Retrieves the X-Content-Type-Options header from an URL string"""
+    r = requests.get(url)
+    return dict(r.headers).get("X-Content-Type-Options")
+
 def test_XFrameOptionsOWASP():
     # Checking that OWASP.org sets the X-Frame-Options header correctly to deny clickjacking
-    assert getXFrameOptions('https://www.owasp.org/') == 'DENY'
+    assert getXFrameOptions('https://owasp.org') == 'DENY'
+
+def test_XXssProtection():
+    assert getXXssProtection('https://owasp.org') != None
+
+def test_XContentTypeOptions():
+    assert getXContentTypeOptions('https://owasp.org') == 'nosniff'
