6d39a9aa9601fc19560cf9301070b1983737cf89
CC <canovac@gmail.com>
2018-10-15 13:23:16 -0700

    added linear search

--- test_string_challenges.py
+++ test_string_challenges.py
@@ -1,8 +1,23 @@
-# Cat two strings, string 'abc' 'xyz' to return: 'a, x, b, y, c, z'
+def linear_search(random_list, val):
+    """ Returns the index of a value 'val' if found in a random list using linear search algorithm """
+    index = 0
+    while index < len(random_list):
+        if random_list[index] == val:
+            return index
+        index += 1
+    return None
 
-def WeaveConcatenate(a, b):
-    print(a)
-    print(b)
 
-def test_CatStrings():
-    raise NotImplementedError()
+def test_linear_search_text():
+    my_list = [1, "a", 20]
+    assert linear_search(my_list, "a") == 1
+
+
+def test_linear_search_number():
+    my_list = [1, "a", 20]
+    assert linear_search(my_list, 20) == 2
+
+
+def test_linear_search_notfound():
+    my_list = [1, "a", 20]
+    assert linear_search(my_list, 0) is None
