a73bf3cfe4d04ed2dee0482c5307f59a4df2da0d
CC <canovac@gmail.com>
2018-10-15 11:54:10 -0700

    Learning python

--- LearningPy.py
+++ LearningPy.py
@@ -1,5 +1,11 @@
+import pytest
+
+
 def fibonacci(n):
     """ Outputs a list of numbers up to and including the given number (if it is a fibonacci number) """
+    if n < 0:
+        raise ValueError("Invalid input", n)
+
     a, b = 0, 1
     my_list = []
     while a <= n:
@@ -9,7 +15,11 @@ def fibonacci(n):
 
 
 def is_fibonacci(n):
-    """ Indicates if the number given is a fibonacci number """
+    """ Indicates if a given number is a fibonacci number """
+    # Quick check for negatives
+    if n < 0:
+        return False
+
     fib_list = fibonacci(n)
     if fib_list[-1] == n:
         return True
@@ -17,6 +27,7 @@ def is_fibonacci(n):
         return False
 
 
+# PyTest tests
 def test_fibonacci_1():
     assert fibonacci(1) == [0, 1, 1]
 
@@ -27,6 +38,11 @@ def test_fibonacci_9():
 
 def test_fibonacci_13():
     assert fibonacci(13) == [0, 1, 1, 2, 3, 5, 8, 13]
+
+
+def test_fibonacci_negative():
+    with pytest.raises(ValueError):
+        fibonacci(-1)
 
 
 def test_is_fibonacci_0():
@@ -43,3 +59,7 @@ def test_is_fibonacci_8():
 
 def test_is_fibonacci_14():
     assert is_fibonacci(14) is False
+
+
+def test_is_fibonacci_negative():
+    assert is_fibonacci(-1) is False
