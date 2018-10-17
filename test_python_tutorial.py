6449e01c3f15046f2505cb7bca9ceb1bf4fc8a85
Christopher Canova <canovac@gmail.com>
2018-10-17 16:17:36 -0700

    adding more on strings

--- test_python_tutorial.py
+++ test_python_tutorial.py
@@ -33,17 +33,38 @@ def python_order_of_operations():
     print("5 + 3 * (2 + 4) / 2", 5 + 3 * (2 + 4) / 2)
 
 
-# Using quotation marks '' and "", escape with \
-quoth = "\"Always Look on the Bright Side of Life\""
-print(quoth)
+def quotes():
+    # Using quotation marks '' and "", escape with \
+    quoth = "\"Always Look on the Bright Side of Life\""
+    print(quoth)
+
+    # Or just use quotes
+    quoth += '\n"This is a dead parrot!"'
+    print(quoth)
 
 
-# Or just use quotes
-quoth += '\n"This is a dead parrot!"'
-print(quoth)
+def embed_a_string():
+    # Embedding a string using %s, which is OG but not great
+    print("%s %s %s %s" % ("What", "is", "your", "name?"))
+
+    # Embedding a string using str.format()
+    print("What is your {}?".format("quest"))
+
+    # Indexing
+    print("What is the {1} of {0} swallow?".format("an unladen", "airspeed velocity"))
+
+    # Advanced, using a dictionary to populate indexes
+    words = {'verb': 'floats', 'predicate': 'water'}
+    print("What also {verb} in {predicate}?".format(**words))
+
+    # Embedding using F strings, simpler
+    name = "Brian"
+    age = "boy"
+    print(f"The {age} they call {name}")
+
+    # upper or lowercase allowed
+    age = "man"
+    print(F"The {age} they call {name}")
 
 
-# Embedding a string
-
-
-# Adapted from https://youtu.be/N4mEzFDjqtA
+# Adapted from https://realpython.com/python-f-strings/#option-1-formatting
